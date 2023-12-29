17a61a95e163e88dae7116ed81df694295b11c0d
Cesar Rodriguez <crcesar15@gmail.com>
2023-12-29 17:05:25 -0400

    Add Breeze package

---
+++ resources/js/login/TheForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-alert show>Default Alert</b-alert>
+</template>
+
+<script>
+export default {
+  name: "TheForm",
+  data() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    onSubmit() {
+      axios
+        .post("/login", {
+          email: this.email,
+          password: this.password
+        })
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          this.errors = error.response.data.errors;
+        });
+    }
+  }
+};
+</script>
