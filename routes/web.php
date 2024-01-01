2611b40c66c9b63f0941aa3a3d7c3b40e75e019f
Cesar Rodriguez <crcesar15@gmail.com>
2023-12-31 21:46:56 -0400

    Add laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/', function () {
     //     return redirect('/login');
     // }
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
