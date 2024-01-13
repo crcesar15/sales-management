320fab7463427ec068e0d943b8cfb3cc58972dc0
Cesar Rodriguez <crcesar15@gmail.com>
2024-01-12 22:17:15 -0400

    Remove bootstrap and add primeflex

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,5 +5,45 @@
  */
 
 import "./bootstrap";
-import "primevue/resources/primevue.css";
-import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
+
+import { createApp } from "vue";
+import PrimeVue from "primevue/config";
+import Menubar from "primevue/menubar";
+
+const app = createApp({
+  components: {
+    Menubar,
+  },
+  data() {
+    return {
+      navbarStyles: {
+        root: ["navbar navbar-expand-lg navbar-dark bg-primary"],
+        menu: ["navbar-nav"],
+        menuitem: ["nav-item"],
+        content: ["nav-link"],
+        action: ["nav-link"],
+      },
+      items: [
+        {
+          label: "Home",
+          icon: "fa fa-home",
+          to: "/",
+        },
+        {
+          label: "Products",
+          icon: "fa fa-shopping-cart",
+          to: "/products",
+        },
+        {
+          label: "About",
+          icon: "fa fa-exclamation-circle",
+          to: "/about",
+        },
+      ],
+    };
+  },
+});
+
+app.use(PrimeVue);
+
+app.mount("#navbar");
