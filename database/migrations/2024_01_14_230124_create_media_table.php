6a53660b553f425f9ccd5e3748a76b9e324e44bb
Cesar Rodriguez <crcesar15@gmail.com>
2024-01-14 19:24:40 -0400

    Add media migration

---
+++ database/migrations/2024_01_14_230124_create_media_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->integer('model_id');
+            $table->string('model');
+            $table->string('filename');
+            $table->json('meta')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('media');
+    }
+};
