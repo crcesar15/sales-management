dbbb19871f1a1bc3450439fc451cca4688fa7c9c
Cesar Rodriguez <crcesar15@gmail.com>
2024-01-14 21:54:22 -0400

    Add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory(1)->create([
+            'name' => 'Cesar Rodriguez',
+            'email' => 'crcesar15@gmail.com',
+            'password' => bcrypt('sample123'),
+        ]);
         $this->call([ProductsSeeder::class]);
     }
 }
