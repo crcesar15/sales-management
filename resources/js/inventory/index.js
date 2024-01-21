8c878dbc7c7ad2b72835ef21f10cec6de174f716
Cesar Rodriguez <crcesar15@gmail.com>
2024-01-21 00:58:26 -0400

    Add vue router

--- resources/js/inventory/index.js
+++ resources/js/inventory/index.js
@@ -1,15 +1,23 @@
 import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 import PrimeVue from "primevue/config";
 import ToastService from "primevue/toastservice";
 import ConfirmationService from "primevue/confirmationservice";
 import ItemList from "./components/ItemList.vue";
+import ItemEditor from "./components/ItemEditor.vue";
 
-const app = createApp({
-  components: {
-    ItemList,
-  },
+const routes = [
+  { path: "/products", component: ItemList },
+  { path: "/products/:id/edit", component: ItemEditor },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
 });
 
-app.use(PrimeVue).use(ToastService).use(ConfirmationService);
+const app = createApp({});
+
+app.use(PrimeVue).use(ToastService).use(ConfirmationService).use(router);
 
 app.mount("#app");
