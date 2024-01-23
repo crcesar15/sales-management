a28a17c46b98c67ec76303a993e2d806e43186f7
Cesar Rodriguez <crcesar15@gmail.com>
2024-01-22 23:59:50 -0400

    Add card component to gallery

--- resources/js/gallery/components/ProductsGallery.vue
+++ resources/js/gallery/components/ProductsGallery.vue
@@ -1,21 +1,49 @@
 <template>
-  <div class="grid">
+  <div class="flex justify-content-around flex-wrap">
     <div
       v-for="product in products"
       :key="product.id"
-      class="sm:col-12 md:col-6 lg:col-4 xl:col-2"
+      class="flex align-items-center justify-content-center"
     >
-      <ProductItem :product="product" />
+      <Card
+        style="width: 20em; min"
+        class="m-5"
+        :pt="{root: ['border-rounded', 'shadow-4']}"
+      >
+        <template #header>
+          <img
+            v-if="product.media.length > 0"
+            :src="product.media[0].url"
+            :alt="product.name"
+            class="w-full p-1"
+          >
+          <div
+            v-else
+            style="height: 20em;"
+            class="flex flex-wrap justify-content-center align-items-center "
+          >
+            <p><i class="fa fa-image" /> No images yet</p>
+          </div>
+        </template>
+        <template #title>
+          {{ product.name }}
+        </template>
+        <template #subtitle>
+          BOB. {{ product.price }}
+        </template>
+      </Card>
     </div>
   </div>
 </template>
 
 <script>
+import Card from "primevue/card";
 import ProductItem from "./ProductItem.vue";
 
 export default {
   components: {
     ProductItem,
+    Card,
   },
   data() {
     return {
