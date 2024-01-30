941cb36630029f7410cf8e6f887004440f8d5696
Cesar Rodriguez <crcesar15@gmail.com>
2024-01-29 21:02:05 -0400

    Add layout

---
+++ resources/js/layouts/admin.vue
@@ -0,0 +1,222 @@
+<template>
+  <div>
+    <div id="navbar">
+      <div>
+        <Sidebar v-model:visible="sidebarVisibility">
+          <template #container="{ closeCallback }">
+            <div class="flex flex-column h-full">
+              <div class="flex align-items-center justify-content-between px-4 pt-3 flex-shrink-0">
+                <span class="inline-flex align-items-center gap-2">
+                  <span
+                    class="font-semibold text-2xl text-primary"
+                  >Laravel</span>
+                </span>
+                <span>
+                  <p-button
+                    type="button"
+                    icon="fa fa-xmark"
+                    rounded
+                    @click="toggleSidebar"
+                  />
+                </span>
+              </div>
+              <div class="overflow-y-auto">
+                <ul class="list-none p-3 m-0">
+                  <li @click="redirect(&quot;/&quot;)">
+                    <a
+                      v-ripple
+                      class="p-3 flex align-items-center hover:surface-100 text-600 cursor-pointer p-ripple"
+                    >
+                      <i class="fas fa-cubes mr-2" />
+                      <span class="font-medium">{{ __('DASHBORAD') }}</span>
+                    </a>
+                  </li>
+                  <li>
+                    <div
+                      v-ripple
+                      v-styleclass="{
+                        selector: '@next',
+                        enterClass: 'hidden',
+                        enterActiveClass: 'slidedown',
+                        leaveToClass: 'hidden',
+                        leaveActiveClass: 'slideup'
+                      }"
+                      class="p-3 flex align-items-center justify-content-between text-600 cursor-pointer p-ripple"
+                    >
+                      <span class="font-medium">
+                        <i class="fa fa-cube pr-2" />PRODUCTS
+                      </span>
+                      <i class="fa fa-angle-down" />
+                    </div>
+                    <ul class="list-none py-0 pl-3 pr-0 m-0 overflow-hidden">
+                      <li @click="redirect(&quot;/gallery&quot;)">
+                        <a
+                          v-ripple
+                          class="flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple"
+                        >
+                          <i class="fa fa-grip mr-2" />
+                          <span class="font-medium">Gallery</span>
+                        </a>
+                      </li>
+                      <li @click="redirect(&quot;/products&quot;)">
+                        <a
+                          v-ripple
+                          class="flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple"
+                        >
+                          <i class="fa fa-box-open mr-2" />
+                          <span class="font-medium">Inventory</span>
+                        </a>
+                      </li>
+                      <li @click="redirect(&quot;/categories&quot;)">
+                        <a
+                          v-ripple
+                          class="flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple"
+                        >
+                          <i class="fa fa-list mr-2" />
+                          <span class="font-medium">Categories</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a
+                      v-ripple
+                      class="p-3 flex align-items-center hover:surface-100 text-600 cursor-pointer p-ripple"
+                    >
+                      <i class="fa fa-dollar-sign mr-2" />
+                      <span class="font-medium">SALES</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      v-ripple
+                      class="p-3 flex align-items-center hover:surface-100 text-600 cursor-pointer p-ripple"
+                    >
+                      <i class="fas fa-wallet mr-2" />
+                      <span class="font-medium">BUYS</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      v-ripple
+                      class="p-3 flex align-items-center hover:surface-100 text-600 cursor-pointer p-ripple"
+                    >
+                      <i class="fa fa-table-list mr-2" />
+                      <span class="font-medium">REPORTS</span>
+                    </a>
+                  </li>
+                  <li>
+                    <div
+                      v-ripple
+                      v-styleclass="{
+                        selector: '@next',
+                        enterClass: 'hidden',
+                        enterActiveClass: 'slidedown',
+                        leaveToClass: 'hidden',
+                        leaveActiveClass: 'slideup'
+                      }"
+                      class="p-3 flex align-items-center justify-content-between text-600 cursor-pointer p-ripple"
+                    >
+                      <span class="font-medium">
+                        <i class="fa fa-gear pr-2" />ADMIN
+                      </span>
+                      <i class="fa fa-angle-down" />
+                    </div>
+                    <ul class="list-none py-0 pl-3 pr-0 m-0 overflow-hidden">
+                      <li @click="redirect(&quot;/users&quot;)">
+                        <a
+                          v-ripple
+                          class="flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple"
+                        >
+                          <i class="fa fa-grip mr-2" />
+                          <span class="font-medium">User</span>
+                        </a>
+                      </li>
+                      <li @click="redirect(&quot;/permissions&quot;)">
+                        <a
+                          v-ripple
+                          class="flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple"
+                        >
+                          <i class="fa fa-box-open mr-2" />
+                          <span class="font-medium">Permissions</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </template>
+        </Sidebar>
+        <Menubar :model="[]">
+          <template #start>
+            <div class="flex flex-row flex-wrap">
+              <div class="flex">
+                <p-button
+                  icon="fa fa-bars"
+                  @click="toggleSidebar"
+                />
+              </div>
+            </div>
+          </template>
+          <template #end>
+            <div class="card flex justify-content-center">
+              <p-button
+                type="button"
+                icon="fa fa-user"
+                aria-haspopup="true"
+                aria-controls="overlay_menu"
+                @click="toggleUserActions"
+              />
+            </div>
+            <p-menu
+              id="overlay_menu"
+              ref="userActions"
+              :model="userActions"
+              :popup="true"
+            />
+          </template>
+        </Menubar>
+      </div>
+    </div>
+    <main class="md:m-4 m-3">
+      <slot />
+    </main>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      sidebarVisibility: true,
+      userActions: [
+        {
+          label: "Profile",
+          icon: "fa fa-user",
+          command: () => {
+            this.$router.push("/profile");
+          },
+        },
+        {
+          label: "Logout",
+          icon: "fa fa-sign-out-alt",
+          command: () => {
+            this.$store.dispatch("auth/logout");
+          },
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.sidebarVisibility = !this.sidebarVisibility;
+    },
+    toggleUserActions() {
+      this.$refs.userActions.toggle();
+    },
+    redirect(path) {
+      this.$router.push(path);
+    },
+  },
+};
+</script>
