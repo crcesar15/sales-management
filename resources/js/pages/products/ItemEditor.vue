ccb2085e22c5a783a09c639d5c892229db503999
Cesar Rodriguez <crcesar15@gmail.com>
2024-02-07 20:54:25 -0400

    Add layout to item editor

--- resources/js/pages/products/ItemEditor.vue
+++ resources/js/pages/products/ItemEditor.vue
@@ -1,89 +1,91 @@
 <template>
-  <div class="p-card">
-    <TabView>
-      <TabPanel header="General Info">
-        <div class="flex justify-content-between flex-wrap">
-          <div class="flex align-items-center justify-content-center">
-            <h3>Item Editor</h3>
+  <AppLayout>
+    <div class="p-card">
+      <TabView>
+        <TabPanel header="General Info">
+          <div class="flex justify-content-between flex-wrap">
+            <div class="flex align-items-center justify-content-center">
+              <h3>Item Editor</h3>
+            </div>
+            <div class="flex align-items-center justify-content-center">
+              <Link
+                href="/products"
+                class="p-button p-button-text"
+              >
+                <i class="fas fa-arrow-left" /> <span>Back</span>
+              </Link>
+            </div>
           </div>
-          <div class="flex align-items-center justify-content-center">
-            <Link
-              href="/products"
-              class="p-button p-button-text"
-            >
-              <i class="fas fa-arrow-left" /> <span>Back</span>
-            </Link>
+          <div class="grid">
+            <div class="col-12 md:col-6 flex flex-column gap-2">
+              <label for="identifier">Identifier</label>
+              <InputText
+                id="identifier"
+                v-model="identifier"
+              />
+            </div>
+            <div class="col-12 md:col-6 flex flex-column gap-2">
+              <label for="name">Name</label>
+              <InputText
+                id="name"
+                v-model="name"
+              />
+            </div>
+            <div class="col-12 flex flex-column gap-2">
+              <label for="description">Description</label>
+              <Textarea
+                id="description"
+                v-model="description"
+              />
+            </div>
+            <div class="col-12 md:col-6 flex flex-column gap-2">
+              <label for="price">Price (Bs.)</label>
+              <InputNumber
+                id="price"
+                v-model="price"
+                :min-fraction-digits="2"
+                :max-fraction-digits="2"
+              />
+            </div>
+            <div class="col-12 md:col-6 flex flex-column gap-2">
+              <label for="stock">Stock</label>
+              <InputNumber
+                id="stock"
+                v-model="stock"
+              />
+            </div>
+            <div class="col-12 md:col-6 flex flex-column gap-2">
+              <label for="brand">Brand</label>
+              <InputText
+                id="brand"
+                v-model="brand"
+              />
+            </div>
+            <div class="col-12 md:col-6 flex flex-column gap-2">
+              <label for="category">Category</label>
+              <InputText
+                id="category"
+                v-model="category"
+              />
+            </div>
+            <div class="col-12 flex justify-content-center mt-2">
+              <PButton
+                label="Save"
+                icon="fas fa-save"
+              />
+            </div>
           </div>
-        </div>
-        <div class="grid">
-          <div class="col-12 md:col-6 flex flex-column gap-2">
-            <label for="identifier">Identifier</label>
-            <InputText
-              id="identifier"
-              v-model="identifier"
-            />
-          </div>
-          <div class="col-12 md:col-6 flex flex-column gap-2">
-            <label for="name">Name</label>
-            <InputText
-              id="name"
-              v-model="name"
-            />
-          </div>
-          <div class="col-12 flex flex-column gap-2">
-            <label for="description">Description</label>
-            <Textarea
-              id="description"
-              v-model="description"
-            />
-          </div>
-          <div class="col-12 md:col-6 flex flex-column gap-2">
-            <label for="price">Price (Bs.)</label>
-            <InputNumber
-              id="price"
-              v-model="price"
-              :min-fraction-digits="2"
-              :max-fraction-digits="2"
-            />
-          </div>
-          <div class="col-12 md:col-6 flex flex-column gap-2">
-            <label for="stock">Stock</label>
-            <InputNumber
-              id="stock"
-              v-model="stock"
-            />
-          </div>
-          <div class="col-12 md:col-6 flex flex-column gap-2">
-            <label for="brand">Brand</label>
-            <InputText
-              id="brand"
-              v-model="brand"
-            />
-          </div>
-          <div class="col-12 md:col-6 flex flex-column gap-2">
-            <label for="category">Category</label>
-            <InputText
-              id="category"
-              v-model="category"
-            />
-          </div>
-          <div class="col-12 flex justify-content-center mt-2">
-            <PButton
-              label="Save"
-              icon="fas fa-save"
-            />
-          </div>
-        </div>
-      </TabPanel>
-      <TabPanel header="Media">
-        <FilesManager
-          :files="media"
-          @upload-file="uploadFile"
-          @delete-file="deleteFile"
-        />
-      </TabPanel>
-    </TabView>
-  </div>
+        </TabPanel>
+        <TabPanel header="Media">
+          <FilesManager
+            :files="media"
+            @upload-file="uploadFile"
+            @delete-file="deleteFile"
+          />
+        </TabPanel>
+      </TabView>
+    </div>
+  </AppLayout>
 </template>
 
 <script>
@@ -94,6 +96,7 @@ import PButton from "primevue/button";
 import TabView from "primevue/tabview";
 import TabPanel from "primevue/tabpanel";
 import FilesManager from "./FilesManager.vue";
+import AppLayout from "../../layouts/admin.vue";
 
 export default {
   components: {
@@ -104,6 +107,7 @@ export default {
     TabView,
     TabPanel,
     FilesManager,
+    AppLayout,
   },
   data() {
     return {
