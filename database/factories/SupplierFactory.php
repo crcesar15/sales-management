590d89178061380a742335e18f4340680106dd8a
Cesar Rodriguez <crcesar15@gmail.com>
2024-05-25 22:04:40 -0400

    Add migration, model and factory for suppliers

---
+++ database/factories/SupplierFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Supplier>
+ */
+class SupplierFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'fullname' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+            'address' => $this->faker->address,
+            'details' => $this->faker->sentence,
+        ];
+    }
+}
