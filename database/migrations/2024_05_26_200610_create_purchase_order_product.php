a9b17d2846bacc8b07b3448107324bf0151cfb40
Cesar Rodriguez <crcesar15@gmail.com>
2024-05-26 16:15:00 -0400

    Add migration for purchase order products pivot table

---
+++ database/migrations/2024_05_26_200610_create_purchase_order_product.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchase_order_product', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('purchase_order_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->float('quantity', 8, 2);
+            $table->float('price', 8, 2);
+            $table->float('total', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchase_order_product');
+    }
+};
