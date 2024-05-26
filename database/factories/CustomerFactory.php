8dd36247bdef8067a83e4e09b420ad5a6fe30b06
Cesar Rodriguez <crcesar15@gmail.com>
2024-05-26 18:01:12 -0400

    Add model. migration and factory for customers

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+            'tax_id' => $this->faker->numerify('##########'),
+            'tax_id_name' => $this->faker->company,
+        ];
+    }
+}
