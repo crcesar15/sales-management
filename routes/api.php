1f79e8ad1bcfa6e116fc418aa5aa709de801666d
Cesar Rodriguez <crcesar15@gmail.com>
2024-09-02 23:34:18 -0400

    Refactor API routes

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\CategoriesController;
+use App\Http\Controllers\Api\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -21,18 +12,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //Routes for Products
 Route::group(['middleware' => 'auth:sanctum', 'as' => 'api.'], function () {
     //Routes for Products
-    Route::get('/products', 'App\Http\Controllers\Api\ProductsController@index');
-    Route::get('/products/{id}', 'App\Http\Controllers\Api\ProductsController@show');
-    Route::post('/products', 'App\Http\Controllers\Api\ProductsController@store');
-    Route::put('/products/{id}', 'App\Http\Controllers\Api\ProductsController@update')->name('products.update');
-    Route::delete('/products/{id}', 'App\Http\Controllers\Api\ProductsController@destroy')->name('products.destroy');
+    Route::get('/products', [ProductsController::class, 'index'])->name('products');
+    Route::get('/products/{id}', [ProductsController::class, 'show'])->name('products.show');
+    Route::post('/products', [ProductsController::class, 'store'])->name('products.store');
+    Route::put('/products/{id}', [ProductsController::class, 'update'])->name('products.update');
+    Route::delete('/products/{id}', [ProductsController::class, 'destroy'])->name('products.destroy');
 
     //Routes for Categories
-    Route::get('/categories', 'App\Http\Controllers\Api\CategoriesController@index');
-    Route::get('/categories/{id}', 'App\Http\Controllers\Api\CategoriesController@show');
-    Route::post('/categories', 'App\Http\Controllers\Api\CategoriesController@store');
-    Route::put('/categories/{id}', 'App\Http\Controllers\Api\CategoriesController@update');
-    Route::delete('/categories/{id}', 'App\Http\Controllers\Api\CategoriesController@destroy');
+    Route::get('/categories', [CategoriesController::class, 'index'])->name('categories');
+    Route::get('/categories/{id}', [CategoriesController::class, 'show'])->name('categories.show');
+    Route::post('/categories', [CategoriesController::class, 'store'])->name('categories.store');
+    Route::put('/categories/{id}', [CategoriesController::class, 'update'])->name('categories.update');
+    Route::delete('/categories/{id}', [CategoriesController::class, 'destroy'])->name('categories.destroy');
 
     //Products Media
     Route::post('/products/media', 'App\Http\Controllers\Api\ProductsMediaController@draft');
