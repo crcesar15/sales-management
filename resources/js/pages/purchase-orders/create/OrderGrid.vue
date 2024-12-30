fdb20042e6987ca19f4a70c742ba60dd493c3d4d
Cesar Rodriguez <crcesar15@gmail.com>
2024-12-30 19:56:09 -0400

    style(Purchase Orders): Change styles of the purchase order grid for dark mode

--- resources/js/pages/purchase-orders/create/OrderGrid.vue
+++ resources/js/pages/purchase-orders/create/OrderGrid.vue
@@ -1,9 +1,39 @@
 <template>
   <div>
     <div v-if="items.length > 0">
-      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <div
+        class="
+          relative
+          overflow-x-auto
+          sm:rounded-lg
+        "
+      >
+        <table
+          class="
+            w-full
+            text-sm
+            text-left
+            rtl:text-right
+            border
+            border-gray-300
+            dark:border-gray-600
+            text-gray-500
+            dark:text-gray-400
+          "
+        >
+          <thead
+            class="
+              text-xs
+              uppercase
+              border
+              border-gray-300
+              dark:border-gray-600
+              bg-gray-50
+              dark:bg-gray-950
+              text-gray-700
+              dark:text-gray-400
+            "
+          >
             <tr>
               <th class="text-left p-4">
                 {{ $t('Product') }}
@@ -26,7 +56,13 @@
             <tr
               v-for="(item, index) in items"
               :key="index"
-              class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+              class="
+                bg-white border-b
+                dark:bg-gray-950
+                dark:border-gray-600
+                hover:bg-gray-50
+                dark:hover:bg-gray-600
+              "
             >
               <td class="w-4 p-4">
                 {{ item.product }}
