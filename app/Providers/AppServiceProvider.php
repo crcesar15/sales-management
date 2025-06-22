5fd84c47912b61258f1bd3c0028f2d41c7d82aaf
Cesar Rodriguez <crcesar15@gmail.com>
2025-06-21 21:27:07 -0400

    feat: Update Debugbar alias registration in configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,6 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $loader = AliasLoader::getInstance();
-        $loader->alias('Debugbar', Barryvdh\Debugbar\Facades\Debugbar::class);
     }
 
     /**
