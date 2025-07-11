61c8c0f0cb517ede7cd00eabcd103bd128dfc254
Cesar Rodriguez <crcesar15@gmail.com>
2025-07-10 21:25:52 -0400

    feat: Add PermissionsController with index method for managing permissions

---
+++ app/Http/Controllers/Api/PermissionsController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ApiCollection;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+
+class PermissionsController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Permission::query();
+
+        $filter = $request->input('filter', '');
+
+        if (!empty($filter)) {
+            $filter = '%' . $filter . '%';
+            $query->where(
+                function ($query) use ($filter) {
+                    $query->where('name', 'like', $filter);
+                }
+            );
+        }
+
+        if ($request->has('select')) {
+            $query->select(explode(',', $request->input('select')));
+        }
+
+        $order_by = $request->has('order_by')
+            ? $order_by = $request->get('order_by')
+            : 'name';
+        $order_direction = $request->has('order_direction')
+            ? $request->get('order_direction')
+            : 'ASC';
+
+        $response = $query->orderBy(
+            $request->input('order_by', $order_by),
+            $request->input('order_direction', $order_direction)
+        )->paginate($request->input('per_page', 10));
+
+        return new ApiCollection($response);
+    }
+}
