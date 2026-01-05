37f701ea5fb04465397fab7e84bbc3b2e693b17d
Cesar Rodriguez <crcesar15@gmail.com>
2026-01-04 21:29:06 -0400

    refactor: Remove 'name' column from product_variants migration

--- database/migrations/2024_05_25_224529_create_product_variants_table.php
+++ database/migrations/2024_05_25_224529_create_product_variants_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->string('identifier', 50)->unique()->nullable();
-            $table->string('name', 150);
             $table->decimal('price', 10, 2);
             $table->integer('stock');
             $table->enum('status', ['active', 'inactive', 'archived'])->default('active');
