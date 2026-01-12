f5c514c177610f60f76857ed14020390b66a066c
Cesar Rodriguez <crcesar15@gmail.com>
2026-01-11 22:34:25 -0400

    refactor: Implement useAuthClient composable for authentication APIs

---
+++ resources/js/Composables/useAuthClient.ts
@@ -0,0 +1,37 @@
+import { useApi } from "@composables/useApi";
+import { route } from "ziggy-js";
+
+export function useAuthClient() {
+    const { apiClient}  = useApi();
+
+    interface loginPayload {
+        username: string;
+        password: string;
+        remember: boolean;
+    }
+
+    const loginApi = async (loginPayload:loginPayload) => {
+        return await apiClient.post(route('login.post'), loginPayload);
+    }
+
+    const requestResetPasswordApi = async (payload: { email: string }) => {
+        return await apiClient.post(route('password.request'), payload);
+    }
+
+    interface resetPasswordPayload {
+        email: string;
+        password: string;
+        password_confirmation: string;
+        token: string;
+    }
+
+    const resetPasswordApi = async (payload: resetPasswordPayload) => {
+        return await apiClient.post(route('password.update'), payload);
+    }
+
+    return {
+        loginApi,
+        resetPasswordApi,
+        requestResetPasswordApi
+    }
+}
