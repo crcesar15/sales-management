485771ebf4cdd1139e8574cb1cf2ef6902352e03
Cesar Rodriguez <crcesar15@gmail.com>
2026-01-18 18:18:23 -0400

    feat: Create migration for store_user table with foreign key constraints

---
+++ database/migrations/2026_01_18_221551_create_store_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('store_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('store_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('store_user');
+    }
+};
