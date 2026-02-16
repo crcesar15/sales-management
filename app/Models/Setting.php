2a936b242ea56cbcb1efe9c1b5146c5df9f700c2
Cesar Rodriguez <crcesar15@gmail.com>
2026-02-16 10:45:34 -0400

    feat: Configure activity logging for the Setting model.

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -5,13 +5,26 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 final class Setting extends Model
 {
+    use LogsActivity;
+
     protected $fillable = [
         'key',
         'value',
         'name',
         'group',
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logFillable()
+            ->logOnlyDirty()
+            ->useLogName('setting')
+            ->dontSubmitEmptyLogs();
+    }
 }
