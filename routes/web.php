de8a32b67099eb6193548520ce369ae84b1ce916
Cesar Rodriguez <crcesar15@gmail.com>
2026-03-22 18:15:36 -0400

    feat: Add routes for roles web CRUD

--- routes/web.php
+++ routes/web.php
@@ -90,7 +90,10 @@ Route::group(['middleware' => ['auth']], function (): void {
     // Role routes
     Route::get('/roles', [RoleController::class, 'index'])->name('roles');
     Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create');
+    Route::post('/roles', [RoleController::class, 'store'])->name('roles.store');
     Route::get('/roles/{role}/edit', [RoleController::class, 'edit'])->name('roles.edit');
+    Route::put('/roles/{role}', [RoleController::class, 'update'])->name('roles.update');
+    Route::delete('/roles/{role}', [RoleController::class, 'destroy'])->name('roles.destroy');
 
     // User Routes
     Route::get('/users', [UserController::class, 'index'])->name('users');
