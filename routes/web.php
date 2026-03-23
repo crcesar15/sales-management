8896836095e16afa007105d8a17f86dbb0903584
Cesar Rodriguez <crcesar15@gmail.com>
2026-03-22 21:33:30 -0400

    feat: Add routes for users web CRUD

--- routes/web.php
+++ routes/web.php
@@ -97,8 +97,12 @@ Route::group(['middleware' => ['auth']], function (): void {
 
     // User Routes
     Route::get('/users', [UserController::class, 'index'])->name('users');
-    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
     Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::put('/users/{user}/restore', [UserController::class, 'restore'])->name('users.restore')->withTrashed();
 
     // Vendors Routes
     Route::get('/vendors', [VendorsController::class, 'index'])->name('vendors');
